e3345cc5012ba36114c0d879ffb22595f8814960
Carl Bourges <carlbrgs77@gmail.com>
2026-03-17 12:08:56 +0100

    removed comments

--- back/Routes/documentRoute.js
+++ back/Routes/documentRoute.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 // Import des fonctions du Controller
 const { uploadDocument, downloadDocument, getDocumentInfo, generateDocument } = require("../Controllers/documentController");
 
-// Use memory storage instead of GridFsStorage for better compatibility
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
