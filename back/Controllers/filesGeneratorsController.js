7d96a12cb89c9eb514281bb696170796ff24e0e9
Carl Bourges <carlbrgs77@gmail.com>
2026-03-18 14:28:05 +0100

    Remove unused files and controllers from the project

--- back/Controllers/filesGeneratorsController.js
+++
@@ -1,9 +0,0 @@
-const getFilesGenerators = async (_req, res) => {
-  return res.status(200).json({
-    message: "Route filesGenerators operationnelle.",
-  });
-};
-
-module.exports = {
-  getFilesGenerators,
-};
